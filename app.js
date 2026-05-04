a80154d260f2df317a9d1a115ad201958028f829
asror0810 <andrewschoolboy484@gmail.com>
2026-05-04 21:29:54 +0500

    Luma Kategoriya Bo'limi HTML,CSS,JS

---
+++ app.js
@@ -0,0 +1,7 @@
+const catMenyu = document.getElementById("categoryMenyu")
+function openMenyu(){
+      return catMenyu.style.display = "block"
+}
+function closeMenyu(){
+    return catMenyu.style.display = "none"
+}
