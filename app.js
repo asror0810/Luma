979fc6651d57859cd94dc290b58fdb1c96c1a724
asror0810 <andrewschoolboy484@gmail.com>
2026-05-05 20:34:09 +0500

    luma HTML CSS JS

--- app.js
+++ app.js
@@ -1,7 +1,71 @@
 const catMenyu = document.getElementById("categoryMenyu")
+const catH1 = document.getElementById("catH1")
+const catTuri = document.getElementById("catTur")
+const catp = document.getElementById("p")
+const catqol = document.getElementById("catqol")
+const catqoll = document.getElementById("catqoll")
 function openMenyu(){
       return catMenyu.style.display = "block"
 }
 function closeMenyu(){
     return catMenyu.style.display = "none"
-}""
+}
+function catNoutbook (){
+    catH1.innerText = "Noutbooklar"
+catTuri.innerText = "Noutbook Brendlari"
+catqol.innerText = "Noutbook kulerlari Soni"
+  catp.innerHTML = `
+   <li>HP</li>
+   <li>Lenovo</li>
+   <li>Asus</li>
+   <li>Acer</li>
+   <li>Macbook</li>
+   <li>Acer</li>
+   <li>Asus</li>
+  `;
+  catqoll.innerHTML = `
+    <li>1 ta kuler</li>
+    <li>2 ta kuler</li>
+    <li>5 ta kuler</li>
+  `
+  catp.style.display = "block"
+    catqoll.style.display = "block"
+  catqol.style.display = "block"
+}
+function catGame(){
+    catH1.innerText = "O'yin Avtomatlari"
+    catTuri.innerText = "O'yin Avtomatlar Brendlari"
+    catp.style.display = "none"
+    catqol.style.display = "none"
+    catqoll.style.display = "none"
+}
+function catKit(){
+    catH1.innerText = "Oshxona uchun Texnikalar"
+    catTuri.innerText = "Oshxona Texnika Turlai"
+     catp.style.display = "none"
+    catqoll.style.display = "none"
+    catqol.style.display = "none"
+}
+function catTel(){
+    catH1.innerText = "Televizorlar"
+    catTuri.innerText = "Televizor Brendlari"
+     catp.style.display = "none"
+    catqoll.style.display = "none"
+    catqol.style.display = "none"
+}
+function catPhone(){
+    catH1.innerText = "Telefonlar"
+    catTuri.innerText = "Telefon Brendlari"
+     catp.style.display = "none"
+    catqol.style.display = "none"
+    catqoll.style.display = "none"
+
+}
+function catBit(){
+    catH1.innerText = "Bitavoy texnikalar"
+    catTuri.innerText = "Bitavoy Texnika Turlari"
+     catp.style.display = "none"
+    catqol.style.display = "none"
+    catqoll.style.display = "none"
+
+}
